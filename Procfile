af359b6e7446be0a48de5fa60de9a1b8e8f5bb91
tony0021074 <tony0021074@gmail.com>
2019-07-11 14:58:08 +0900

    v2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn textrecognizesite.wsgi
--- textrecognizesite/settings.py
+++ textrecognizesite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+
+#for herokuapp
+django_heroku.settings(locals())
