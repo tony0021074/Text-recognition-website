3a2869da4e2e593f8a318067e5930b902433589f
tony0021074 <tony0021074@gmail.com>
2019-07-12 00:00:39 +0900

    v2

--- textrecognizesite/settings.py
+++ textrecognizesite/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
