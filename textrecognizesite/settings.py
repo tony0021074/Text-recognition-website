982e19932ddb95b16d49a4e9c80e3b82d47b3eb7
tony0021074 <tony0021074@gmail.com>
2019-07-11 16:33:12 +0900

    v2

--- textrecognizesite/settings.py
+++ textrecognizesite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'si-p&sbcpp_ua7c5y$2@hvy_dt3br#-rnh&1dmip9%)e!-uypf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
