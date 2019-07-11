37639cad6c77aa464baead0623630a8e7a8a0f45
tony0021074 <tony0021074@gmail.com>
2019-07-12 00:04:55 +0900

    v2

--- textrecognizesite/urls.py
+++ textrecognizesite/urls.py
@@ -27,5 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+'''
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''
